0a2dea259a09d78f0fc05e923c72571e142d7c55
Koen François <koen.francois@showpad.com>
2019-01-27 11:37:15 +0100

    Add antd, CRA configuration overrides for custom ant theme

---
+++ src/app/app.container.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+import React, { Component, Fragment } from 'react';
+
+export class AppContainer extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Button type="primary">button</Button>
+        </header>
+      </div>
+    );
+  }
+}
