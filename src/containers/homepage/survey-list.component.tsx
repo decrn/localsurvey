abaa671cc904fc3b88c251a674eb267e0feb6b72
Tuur Lievens <tuur.lievens@hotmail.com>
2019-02-10 00:07:39 +0100

    Improve responsiveness of surveylist close #42

--- src/containers/homepage/survey-list.component.tsx
+++ src/containers/homepage/survey-list.component.tsx
@@ -5,10 +5,12 @@ import RadioGroup from 'antd/lib/radio/group';
 import RadioButton from 'antd/lib/radio/radioButton';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { CollapsibleButtons } from '../../common/components/collapsible-buttons/collapsible-buttons.component';
 import { StatusTag } from '../../common/components/status-tag/status-tag.component';
 import { mapStatusToIcon } from '../../common/mappers/survey-status.mapper';
 import { SurveyStatus } from '../../common/types/survey-status.type';
 import { Survey, surveyDateToLocaleString } from '../../common/types/survey.type';
+import './survey-list.component.less';
 
 export interface SurveyListProps {
     items: Survey[];
@@ -69,79 +71,84 @@ export class SurveyList extends Component<SurveyListProps> {
             },
             {
                 title: 'Actions',
+                className: 'action-buttons',
                 render: () => (
                     <>
-                        <Button type="primary" icon="form">
-                            Start surveying
-                        </Button>
-                        <Button type="default" icon="eye-o">
-                            View details
-                        </Button>
-                        <Button type="danger" icon="delete">
-                            Delete
-                        </Button>
+                        <CollapsibleButtons>
+                            <Button type="primary" icon="form" onClick={() => console.log('Test 1')}>
+                                Start surveying
+                            </Button>
+                            <Button type="default" icon="eye-o" onClick={() => console.log('Test 2')}>
+                                View details
+                            </Button>
+                            <Button type="danger" icon="delete" onClick={() => console.log('Test 3')}>
+                                Delete
+                            </Button>
+                        </CollapsibleButtons>
                     </>
                 ),
             },
         ];
 
         return (
-            <>
-                <Row type="flex" justify="space-between">
-                    <Col span={12}>
-                        <h2>Surveys</h2>
-                    </Col>
-                    <Col span={12}>
-                        <Link to="/survey/new">
-                            <Tooltip placement="left" title="Create a new survey">
-                                <Button type="primary" style={{ float: 'right' }}>
-                                    <Icon type="plus" />
-                                    New survey
-                                </Button>
-                            </Tooltip>
-                        </Link>
-                    </Col>
-                </Row>
-                <Table
-                    dataSource={this.filter(items)}
-                    columns={columns}
-                    bordered
-                    title={() => (
-                        <Row type="flex" justify="space-between">
-                            <Col span={16}>
-                                <span>Filter: &nbsp;</span>
-                                <RadioGroup
-                                    name="status-filter"
-                                    defaultValue="all"
-                                    onChange={(e: RadioChangeEvent) => {
-                                        this.setState({ ...this.state, statusFilter: e.target.value });
-                                    }}
-                                >
-                                    <RadioButton value="all">All</RadioButton>
-                                    <RadioButton value={SurveyStatus.Published}>
-                                        <Icon type={mapStatusToIcon(SurveyStatus.Published)} /> Published
-                                    </RadioButton>
-                                    <RadioButton value={SurveyStatus.InProgress}>
-                                        <Icon type={mapStatusToIcon(SurveyStatus.InProgress)} /> Drafts
-                                    </RadioButton>
-                                    <RadioButton value={SurveyStatus.Cancelled}>
-                                        <Icon type={mapStatusToIcon(SurveyStatus.Cancelled)} /> Cancelled
-                                    </RadioButton>
-                                </RadioGroup>
-                            </Col>
-                            <Col span={8}>
-                                <Search
-                                    style={{ float: 'right' }}
-                                    placeholder="Search surveys..."
-                                    onChange={e => this.setState({ ...this.state, search: e.target.value })}
-                                    enterButton
-                                    allowClear
-                                />
-                            </Col>
-                        </Row>
-                    )}
-                />
-            </>
+            <div className="table-container">
+                <div className="table-scroll">
+                    <Row type="flex" justify="space-between">
+                        <Col span={12}>
+                            <h2>Surveys</h2>
+                        </Col>
+                        <Col span={12}>
+                            <Link to="/survey/new">
+                                <Tooltip placement="left" title="Create a new survey">
+                                    <Button type="primary" style={{ float: 'right' }}>
+                                        <Icon type="plus" />
+                                        New survey
+                                    </Button>
+                                </Tooltip>
+                            </Link>
+                        </Col>
+                    </Row>
+                    <Table
+                        dataSource={this.filter(items)}
+                        columns={columns}
+                        bordered
+                        title={() => (
+                            <Row type="flex" justify="space-between">
+                                <Col span={16}>
+                                    <span>Filter: &nbsp;</span>
+                                    <RadioGroup
+                                        name="status-filter"
+                                        defaultValue="all"
+                                        onChange={(e: RadioChangeEvent) => {
+                                            this.setState({ ...this.state, statusFilter: e.target.value });
+                                        }}
+                                    >
+                                        <RadioButton value="all">All</RadioButton>
+                                        <RadioButton value={SurveyStatus.Published}>
+                                            <Icon type={mapStatusToIcon(SurveyStatus.Published)} /> Published
+                                        </RadioButton>
+                                        <RadioButton value={SurveyStatus.InProgress}>
+                                            <Icon type={mapStatusToIcon(SurveyStatus.InProgress)} /> Drafts
+                                        </RadioButton>
+                                        <RadioButton value={SurveyStatus.Cancelled}>
+                                            <Icon type={mapStatusToIcon(SurveyStatus.Cancelled)} /> Cancelled
+                                        </RadioButton>
+                                    </RadioGroup>
+                                </Col>
+                                <Col span={8}>
+                                    <Search
+                                        style={{ float: 'right' }}
+                                        placeholder="Search surveys..."
+                                        onChange={e => this.setState({ ...this.state, search: e.target.value })}
+                                        enterButton
+                                        allowClear
+                                    />
+                                </Col>
+                            </Row>
+                        )}
+                    />
+                </div>
+            </div>
         );
     }
 }
