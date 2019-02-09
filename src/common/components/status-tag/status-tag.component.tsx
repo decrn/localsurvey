0ebd60c3a02bcf2b1c040927f9cef0eccbd86f6d
Denis Carnier <denis_carn@hotmail.com>
2019-02-09 17:38:57 +0100

    Create funky StatusTag variations

--- src/common/components/status-tag/status-tag.component.tsx
+++ src/common/components/status-tag/status-tag.component.tsx
@@ -1,13 +1,20 @@
 import { Icon, Tag, Tooltip } from 'antd';
-import React, { Component } from 'react';
+import React, { Component, CSSProperties } from 'react';
 import { mapStatusToColor } from '../../mappers/survey-status.mapper';
 import { SurveyStatus } from '../../types/survey-status.type';
 
 export interface StatusTagProps {
     status: SurveyStatus;
+    extended?: boolean;
+    size?: string;
 }
 
 export class StatusTag extends Component<StatusTagProps> {
+    static defaultProps: Partial<StatusTagProps> = {
+        extended: false,
+        size: 'medium',
+    };
+
     mapStatusToTooltip = (status: SurveyStatus): string => {
         switch (status) {
             case SurveyStatus.Published:
@@ -35,8 +42,20 @@ export class StatusTag extends Component<StatusTagProps> {
         }
     };
 
+    mapSizeToStyle = (size: string): CSSProperties => {
+        switch (size) {
+            case 'small':
+                return { padding: '0px 6px 10px', fontSize: 10 };
+            case 'large':
+                return { padding: '6px 9px 24px', fontSize: 15 };
+            default:
+            case 'medium':
+                return { padding: '3px 7px 22px', fontSize: 13 };
+        }
+    };
+
     render() {
-        const { status } = this.props;
+        const { status, size, extended } = this.props;
 
         const color = mapStatusToColor(status);
         const tooltip = this.mapStatusToTooltip(status);
@@ -44,8 +63,9 @@ export class StatusTag extends Component<StatusTagProps> {
 
         return (
             <Tooltip placement="top" title={tooltip}>
-                <Tag color={color}>
+                <Tag style={this.mapSizeToStyle(size!)} color={color}>
                     <Icon type={icon} />
+                    {extended ? <span style={{ paddingLeft: '5px' }}>{status}</span> : null}
                 </Tag>
             </Tooltip>
         );
