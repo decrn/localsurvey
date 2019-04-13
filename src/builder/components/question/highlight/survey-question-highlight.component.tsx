84cd8b9b60f5fc62fe7339c39a18051b6dd75ccf
Denis Carnier <denis_carn@hotmail.com>
2019-04-13 23:52:52 +0200

    Add docs to SurveyQuestionHighlight

--- src/builder/components/question/highlight/survey-question-highlight.component.tsx
+++ src/builder/components/question/highlight/survey-question-highlight.component.tsx
@@ -2,20 +2,25 @@ import { Icon } from 'antd';
 import React, { SFC } from 'react';
 import { SurveyItemType } from '../../../../common/types/survey-item.type';
 
-/**
- * Displays a colorful decoration on the survey question card with an
- * appropriate icon, depending on the survey question type
- */
-
 type HighlightMap = {
     [key in SurveyItemType]: {
+        /** Icon taken from antd
+         * @see https://ant.design/components/icon/ */
         icon: string;
+        /** BG color taken from the '50' row of MaterialUI colors
+         * @see https://www.materialui.co/colors */
         backgroundColor: string;
+        /** Icon color taken from the '200' row of MaterialUI colors
+         * @see https://www.materialui.co/colors */
         iconColor: string;
     }
 };
 
-const highlights: HighlightMap = {
+// TODO: Even though this has proper typing and will complain when new SurveyItemTypes are added
+// and their styling has not yet been defined, it might still be worth to more tightly couple
+// this with SurveyItemType, in the expectation that theming will one day be centralized
+// and these colors and icons will be reused throughout. Or you know, just keep it here...
+const highlightMap: HighlightMap = {
     single: {
         icon: 'swap',
         backgroundColor: '#E3F2FD',
@@ -31,25 +36,22 @@ const highlights: HighlightMap = {
         backgroundColor: '#E0F2F1',
         iconColor: '#80CBC4',
     },
-    // Ideally I would like to do something like
-    // SurveyItemType.SingleChoice: {
-    //     ...
-    //     icon: 'foo',
-    //     ...
-    // }
-    // But TS has let me down :(
-    // See https://github.com/Microsoft/TypeScript/issues/24220
 };
 
 export interface SurveyQuestionHighlightProps {
     type: SurveyItemType;
 }
 
+/**
+ * Displays a colorful decoration on the survey question card with an appropriate icon, depending on the survey question type
+ *
+ * @param type the SurveyItemType to render a highlight for
+ */
 // TODO: maybe turn this into a React.Memo ?
 export const SurveyQuestionHighlight: SFC<SurveyQuestionHighlightProps> = ({
     type,
 }: SurveyQuestionHighlightProps): JSX.Element => (
-    <div className="question-highlight" style={{ backgroundColor: highlights[type].backgroundColor }}>
-        <Icon style={{ color: highlights[type].iconColor }} type={highlights[type].icon} />
+    <div className="question-highlight" style={{ backgroundColor: highlightMap[type].backgroundColor }}>
+        <Icon style={{ color: highlightMap[type].iconColor }} type={highlightMap[type].icon} />
     </div>
 );
