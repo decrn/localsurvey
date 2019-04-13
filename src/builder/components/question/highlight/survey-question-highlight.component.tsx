dacec76ec71515def7e1b615b44bf362a7f137a2
Denis Carnier <denis_carn@hotmail.com>
2019-04-13 20:28:05 +0200

    Update SurveyQuestionCardHighlight
    Type and refactor highlight map

--- src/builder/components/question/highlight/survey-question-highlight.component.tsx
+++ src/builder/components/question/highlight/survey-question-highlight.component.tsx
@@ -7,35 +7,49 @@ import { SurveyItemType } from '../../../../common/types/survey-item.type';
  * appropriate icon, depending on the survey question type
  */
 
+type HighlightMap = {
+    [key in SurveyItemType]: {
+        icon: string;
+        backgroundColor: string;
+        iconColor: string;
+    }
+};
+
+const highlights: HighlightMap = {
+    single: {
+        icon: 'swap',
+        backgroundColor: '#E3F2FD',
+        iconColor: '#90CAF9',
+    },
+    multi: {
+        icon: 'bars',
+        backgroundColor: '#ffebee',
+        iconColor: '#ef9a9a',
+    },
+    open: {
+        icon: 'message',
+        backgroundColor: '#E0F2F1',
+        iconColor: '#80CBC4',
+    },
+    // Ideally I would like to do something like
+    // SurveyItemType.SingleChoice: {
+    //     ...
+    //     icon: 'foo',
+    //     ...
+    // }
+    // But TS has let me down :(
+    // See https://github.com/Microsoft/TypeScript/issues/24220
+};
+
 export interface SurveyQuestionHighlightProps {
     type: SurveyItemType;
 }
 
+// TODO: maybe turn this into a React.Memo ?
 export const SurveyQuestionHighlight: SFC<SurveyQuestionHighlightProps> = ({
     type,
-}: SurveyQuestionHighlightProps): JSX.Element => {
-    // https://ant.design/components/icon/
-    const icons = {
-        single: 'swap',
-        multi: 'bars',
-        open: 'message',
-    };
-
-    const color = {
-        single: '#E3F2FD',
-        multi: '#ffebee',
-        open: '#E0F2F1',
-    };
-
-    const iconColors = {
-        single: '#90CAF9',
-        multi: '#ef9a9a',
-        open: '#80CBC4',
-    };
-
-    return (
-        <div className="question-highlight" style={{ backgroundColor: color[type] }}>
-            <Icon style={{ color: iconColors[type] }} type={icons[type] || 'question'} />
-        </div>
-    );
-};
+}: SurveyQuestionHighlightProps): JSX.Element => (
+    <div className="question-highlight" style={{ backgroundColor: highlights[type].backgroundColor }}>
+        <Icon style={{ color: highlights[type].iconColor }} type={highlights[type].icon} />
+    </div>
+);
